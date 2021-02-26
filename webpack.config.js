d1ec0c73788f7fe7027b764be3bdc2406908313c
kbvv <342768292@qq.com>
2021-02-26 17:32:57 +0800

    updata

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js'
-  }
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+        title: 'llw',
+        template: 'src/assets/index.html'
+      }
+  )],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
+
