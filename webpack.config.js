24492f2724e8bb299a6c5164fa3d3683aeecfc31
kbvv <342768292@qq.com>
2021-03-01 21:21:04 +0800

    last

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,23 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
+const path = require("path");
+
+const base = require('./webpack.config.base.js')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  ...base,
+  devtool: "inline-source-map",
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist"
   },
-  entry: './src/index.js',
-  output: {
-    filename: 'index.[contenthash].js'
-  },
-  plugins: [new HtmlWebpackPlugin(
-    {
-        title: 'llw',
-        template: 'src/assets/index.html'
-      }),
-      new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css',
-        chunkFilename: '[id].[contenthash].css',
-        ignoreOrder:false,
-      }),
-    ],
   module: {
     rules: [
+      ...base.module.rules,
       {
         test: /\.css$/i,
-        //use: ["style-loader", "css-loader"],
-        use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                publicPath: '../',
-                //hmr:process.env.NODE_ENV==='development',
-              },
-            },
-            'css-loader',
-          ],
-      },
-    ],
-  },
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
 
